b7a7f1425910cb393dcfad99eaaf721f92954466
Noor Shabib <noorshabib@wifibridge-54.home>
2024-08-04 01:19:15 +0300

    feat: add assets

---
+++ src/assets/index.ts
@@ -0,0 +1,11 @@
+import EyeIcon from "./eyeIcon";
+import ForkIcon from "./forkIon";
+import ArrowDownIcon from "./arrowDownIcon";
+import Empty from "./empty.png";
+
+export {
+    EyeIcon,
+    ForkIcon,
+    ArrowDownIcon,
+    Empty,
+}
